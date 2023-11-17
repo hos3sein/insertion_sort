c4bb2f16a2fd9bd3ed6c283aa92df70b9a936c57
hossein <kianlucifer0098@gmail.com>
2023-11-16 22:43:17 -0800

    done

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,18 +1,19 @@
+x_list = [8,5,7,9,2,4,6,3]
 
-x_list = [5,8,6,7,9,2,4]
-
-def insertion_sort(x_list):
+def insertion_sort(listt):
 	sorted_list=[]
-	while True:
-		sorted_list.append(min(x_list))
-		x_list.remove(min(x_list))
-		if len(x_list) == 1:
-			sorted_list.append(x_list[0])
-			break
-
+	sorted_list.append(listt[0])
+	for i in range(1 , len(listt)):
+		sorting = listt[i]
+		for j in range(len(sorted_list)):
+			if sorting < sorted_list[j]:
+				sorted_list.insert(j , sorting)
+				break
+			elif j == (len(sorted_list)-1):
+				sorted_list.append(sorting)
 	return(sorted_list)
 
 
-answer = insertion_sort(x_list)
 
-print(answer)
+
+print(insertion_sort(x_list))
